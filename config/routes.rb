8753aa1a6b93d87a10b90fa2b4981145b48a42fc
Reid South <rbsouth@gmail.com>
2018-03-23 11:51:45 -0600

    routes basic stated

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,11 @@
 Rails.application.routes.draw do
-  get 'groupies/create'
-  get 'groupies/destroy'
-  get 'challenges/index'
-  get 'challenges/show'
-  get 'challenges/new'
-  get 'challenges/create'
-  get 'challenges/edit'
-  get 'challenges/update'
-  get 'challenges/destroy'
-  get 'groups/index'
-  get 'groups/show'
-  get 'groups/new'
-  get 'groups/create'
-  get 'groups/edit'
-  get 'groups/update'
-  get 'groups/destroy'
-  get 'friendships/create'
-  get 'friendships/destroy'
-  get 'users/index'
-  get 'users/show'
+
   devise_for :users
+ 	resources :users, only: [:index, :show]
+ 	resources :groups
+ 	resources :freindships, only: [:create, :destroy]
+ 	resources :groupies, only: [:create, :destroy]
+ 	resources :challenges
 	root "home#index" 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
