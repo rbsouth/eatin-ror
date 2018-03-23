d7455f80c059526b20fcd085c2240dfde3ba2c68
Reid South <rbsouth@gmail.com>
2018-03-23 16:50:27 -0600

    styling and js imports required set up

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,22 +1,43 @@
 class GroupsController < ApplicationController
   def index
+    @groups = Group.all
   end
 
   def show
+    @group = Group.find(params[:id])
   end
 
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      puts "yay"
+      redirect_to @group
+    else
+      puts "noooooo"
+      render :new
+    end
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    @group.update_attributes(group_params)
   end
 
   def destroy
+    Group.find(params[:id]).destroy
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:user_id, :name)
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,5 @@
 class Group < ApplicationRecord
 	has_many :users, through: :groupies
+
+	validates :user_id, :name, presence: true
 end
