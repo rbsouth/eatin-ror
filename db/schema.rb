82c7b87f72ffd0544d3d6d32b1e30cb4d9a1c237
Reid South <rbsouth@gmail.com>
2018-03-23 11:28:15 -0600

    friendships accepted false defualt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_23_170930) do
+ActiveRecord::Schema.define(version: 2018_03_23_171938) do
 
   create_table "challenges", force: :cascade do |t|
     t.string "location"
@@ -26,9 +26,9 @@ ActiveRecord::Schema.define(version: 2018_03_23_170930) do
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "friend_id"
-    t.boolean "accepted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "accepted", default: false
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
