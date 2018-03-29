9b2300b7783ab34f0fbf576858e0e1e271ecd202
Reid South <rbsouth@gmail.com>
2018-03-29 14:22:19 -0600

    image added to folder

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   has_many :groupies
   has_many :groups, through: :groupies
   has_many :created_groups, class_name: "Group", foreign_key: "user_id"
-  has_many :friendships
-	has_many :friends, through: :friendships
   has_many :invites
+  has_many :friendships
+  has_many :friends, through: :friendships
 	has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
 	has_many :inverse_friends, through: :inverse_friendships, source: :user
   
