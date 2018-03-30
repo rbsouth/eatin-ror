96bb1afe3e798307a869ab64e5b2692a3d090b3e
Reid South <rbsouth@gmail.com>
2018-03-30 01:33:55 -0600

    added dependancies in models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,8 @@
 class Group < ApplicationRecord
-	has_many :groupies
+	has_many :groupies, dependent: :destroy
 	has_many :users, through: :groupies
 
 	validates :name, :central_location, :latitude, :longitude, presence: true
 	validates :name, uniqueness: { scope: [:user_id] }
-	validates :latitude, :longitude, numericality: { only_integer: true }
+	# validates :latitude, :longitude, numericality: { only_integer: true }
 end
--- db/migrate/20180330045050_change_lat_and_lng_to_decimals.rb
+++
@@ -1,8 +0,0 @@
-class ChangeLatAndLngToDecimals < ActiveRecord::Migration[5.2]
-  def change
-  	remove_column :groups, :latitude, 
-  	remove_column :groups, :longitude, 
-  	add_column :groups, :latitude, :decimal
-  	add_column :groups, :longitude, :decimal
-  end
-end
