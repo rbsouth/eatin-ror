7b13c5c443c11c0d7f7136d653f4fb027d1f2d8a
Reid South <rbsouth@gmail.com>
2018-03-29 22:17:23 -0600

    removed user_id validation for testing

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
 	has_many :groupies
 	has_many :users, through: :groupies
 
-	validates :name, :user_id, :central_location, :latitude, :longitude, presence: true
+	validates :name, :central_location, :latitude, :longitude, presence: true
 	validates :name, uniqueness: { scope: [:user_id] }
 	validates :latitude, :longitude, numericality: { only_integer: true }
 end
