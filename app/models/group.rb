ea05ace03c5e906b5701796990441392d107bb69
Reid South <rbsouth@gmail.com>
2018-03-30 03:34:58 -0600

    added before validation to group that sets a max radius

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,8 +2,17 @@ class Group < ApplicationRecord
 	has_many :groupies, dependent: :destroy
 	has_many :users, through: :groupies
 
+	before_validation :radius_max
+
 	validates :name, :central_location, :latitude, :longitude, :radius, presence: true
 	validates :name, uniqueness: { scope: [:user_id] }
 	validates :radius, numericality: { only_integer: true, greater_than_or_equal_to: 5, less_than_or_equal_to: 81 }
-	# validates :latitude, :longitude, numericality: { only_integer: true }
+	validates :latitude, :longitude, numericality: { only_integer: true }
+
+	def radius_max
+		if :radius > 81
+			:radius = 80
+		end
+	end
+
 end
