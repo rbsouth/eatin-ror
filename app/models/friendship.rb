a0f9b1188c0f5df6db919fc744b225d5f50351db
Reid South <rbsouth@gmail.com>
2018-04-09 01:59:41 -0600

    fixed bug preventing frindship request declining

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,7 @@
 class Friendship < ApplicationRecord
   belongs_to :user
 	belongs_to :friend, :class_name => "User"
+
+	validates :user_id, :friend_id, :accepted, presence: true
+	validates :user_id, uniqueness: { scope: [:friend_id] }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
  	resources :users, only: [:index, :show]
  	resources :groups
- 	resources :friendships, only: [:create, :destroy, :index]
+ 	resources :friendships
  	resources :groupies, only: [:create, :update, :destroy]
- 	resources :invites, only: [:create, :destroy]
  	resources :challenges
 	root "groups#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
